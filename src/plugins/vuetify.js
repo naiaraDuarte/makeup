7424f58bff50b31bb35896dc7d1b574894a3260e
naiaraDuarte <naiara.duarte123@gmail.com>
2021-08-04 07:45:18 -0300

    usando padrão de cores

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,17 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
+import colors from './lib/colors'
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                primary: colors.shades.black, // #E53935
+                secondary: colors.red.lighten4, // #FFCDD2
+                accent: colors.indigo.base, // #3F51B5
+            },
+        },
+    },
 });
