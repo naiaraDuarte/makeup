936a95adac8dfec90c4bdd55ecf518a4bafa7b93
naiaraDuarte <naiara.duarte123@gmail.com>
2021-08-05 19:53:38 -0300

    Alteração vuetify

--- src/components/layout/navbar.vue
+++ src/components/layout/navbar.vue
@@ -1,4 +1,59 @@
 <template>
+<!-- <div id="app">
+  <v-app id="inspire">
+    <div class="ma-12 pa-12">
+      <v-card>
+        <v-navigation-drawer
+          permanent
+          expand-on-hover
+        >
+          <v-list>
+            <v-list-item class="px-2">
+              <v-list-item-avatar>
+                <v-img src="https://randomuser.me/api/portraits/women/85.jpg"></v-img>
+              </v-list-item-avatar>
+            </v-list-item>
+  
+            <v-list-item link>
+              <v-list-item-content>
+                <v-list-item-title class="text-h6">
+                  Sandra Adams
+                </v-list-item-title>
+                <v-list-item-subtitle>sandra_a88@gmail.com</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+  
+          <v-divider></v-divider>
+  
+          <v-list
+            nav
+            dense
+          >
+            <v-list-item link>
+              <v-list-item-icon>
+                <v-icon>mdi-folder</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>My Files</v-list-item-title>
+            </v-list-item>
+            <v-list-item link>
+              <v-list-item-icon>
+                <v-icon>mdi-account-multiple</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Shared with me</v-list-item-title>
+            </v-list-item>
+            <v-list-item link>
+              <v-list-item-icon>
+                <v-icon>mdi-star</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Starred</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-navigation-drawer>
+      </v-card>
+    </div>
+  </v-app>
+</div> -->
   <nav>
     <v-toolbar flat app class="navbar black">
       <v-toolbar-side-icon
@@ -11,8 +66,6 @@
         height="43"
       />
       <v-spacer></v-spacer>
-
-      <!-- dropdown menu -->
       <v-menu offset-y>
         <v-btn flat slot="activator" color="grey">
           <v-icon left>expand_more</v-icon>
@@ -34,7 +87,7 @@
         <span>Sign Out</span>
         <v-icon right>exit_to_app</v-icon>
       </v-btn>
-    </v-toolbar>
+    </v-toolbar> -->
 
     <v-navigation-drawer app v-model="drawer" class="primary">
       <v-layout column align-center>
@@ -56,7 +109,7 @@
           :to="link.route"
         >
           <v-list-tile-action>
-            <v-icon class="white--text">{{ link.icon }}</v-icon>
+            <v-icon class="" color="#b38b57">{{ link.icon }}</v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
             <v-list-tile-title class="white--text">{{
@@ -66,11 +119,61 @@
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
-  </nav>
+
+    <!-- <v-navigation-drawer
+        permanent
+        expand-on-hover 
+      >
+        <v-list>
+          <v-list-item class="px-2">
+            <v-list-item-avatar>
+              <v-img src="https://randomuser.me/api/portraits/women/85.jpg"></v-img>
+            </v-list-item-avatar>
+          </v-list-item>
+
+          <v-list-item link>
+            <v-list-item-content>
+              <v-list-item-title class="text-h6">
+                Sandra Adams
+              </v-list-item-title>
+              <v-list-item-subtitle>sandra_a88@gmail.com</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-list
+          nav
+          dense
+        >
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>mdi-folder</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>My Files</v-list-item-title>
+          </v-list-item>
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>mdi-account-multiple</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Shared with me</v-list-item-title>
+          </v-list-item>
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>mdi-star</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Starred</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer> -->
+
+   </nav>
 </template>
 
 <script>
 export default {
+  el: '#app',
   data() {
     return {
       drawer: false,
@@ -92,6 +195,7 @@ export default {
 }
 .navbar {
   /* color: #444240; */
+  color: #b38b57;
   background-image: linear-gradient(to bottom, black, black, black,black,black,black,#deb887);
 }
 </style>
