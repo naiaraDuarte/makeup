cec52caf6e83312590bd6d8974f4622a8bbe11db
naiaraDuarte <naiara.duarte123@gmail.com>
2021-08-06 23:09:05 -0300

    Inclusão form cadastro parte 1

--- src/components/pages/produtos/cadastro.vue
+++ src/components/pages/produtos/cadastro.vue
@@ -1,24 +1,111 @@
 <template>
-<p>Dentro de cadastro</p>
-  <!-- <div class="frames-content">
+  <div class="frame mt-5">
     <v-container fluid>
       <v-row>
-        <v-col lg="2" md="12" sm="12" cols="12">
-            
+        <v-col lg="12" md="12" sm="12" cols="12">
+          <v-card elevation="0" v-if="faseCadastro == 0">
+            <h2 class="cor-letra text-center mt-5 pt-5">
+              Vamos lá, me informe os dados do produto...
+            </h2>
+            <v-row class="mt-5 mx-4 pt-5">
+              <v-col lg="4">
+                <v-text-field
+                  v-model="codProduto"
+                  :counter="10"
+                  type="number"
+                  label="Digite o código do produto"
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col lg="4">
+                <v-text-field
+                  v-model="nomeProduto"
+                  :counter="10"
+                  label="Digite o nome do produto"
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col lg="4">
+                <v-text-field
+                  v-model="descProduto"
+                  :counter="10"
+                  label="Digite a descrição do produto"
+                  required
+                ></v-text-field>
+              </v-col>
+            </v-row>
+            <v-row class="mt-5 mx-3 my-3">
+              <v-col lg="4">
+                <!-- Colocar um auto complete -->
+                <v-text-field
+                  :counter="10"
+                  label="Digite a marca do produto"
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col lg="4">
+                <v-text-field
+                  v-model="categoriaProduto"
+                  :counter="10"
+                  label="Digite a categoria do produto"
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col lg="4">
+                <v-text-field
+                  v-model="tipoProduto"
+                  :counter="10"
+                  label="Digite o tipo do produto"
+                  required
+                ></v-text-field>
+              </v-col>
+            </v-row>
+          </v-card>
+          <div v-else>Teste 2</div>
+        </v-col>
+      </v-row>
+      <v-row class="text-right" style="margin-left: 50px;">
+        <v-col lg="11" class="text-right pb-5 mb-5">
+          <v-btn
+            elevation="3"
+            color="white"
+            class="btnSubmit"
+            @click="trocaValores()"
+            >Continuar</v-btn
+          >
         </v-col>
       </v-row>
     </v-container>
-  </div> -->
+  </div>
 </template>
 
 <script>
-
 export default {
-  components: {
-  },
+  components: {},
   data() {
     return {
+      faseCadastro: 0,
+      codProduto: "",
+      nomeProduto: "",
+      descProduto: "",
+      categoriaProduto: "",
+      tipoProduto: "",
     };
+  },
+  methods: {
+    trocaValores() {
+      this.faseCadastro == 0
+        ? (this.faseCadastro = 1)
+        : (this.faseCadastro = 0);
+    },
   },
 };
 </script>
+<style scoped>
+.frame {
+  box-shadow: 0 0 15px #ddd;
+}
+.btnSubmit{
+  color: #b38b57;
+}
+</style>
--- src/views/produtos.vue
+++ src/views/produtos.vue
@@ -1,27 +1,44 @@
 <template>
   <div>
-    <h3>Produtos</h3>
-    <v-btn elevation="8" v-if="componente === 'cadastro'" @click="componente = 'visualizacao'">Visualização</v-btn>
-    <v-btn elevation="8" v-if="componente === 'visualizacao'" @click="componente = 'cadastro'">Cadastro</v-btn>
+    <v-row class="mt-5">
+      <v-col lg="10">
+        <h1 class="namePage">
+          <v-icon x-large>mdi-chevron-double-right</v-icon> Produtos
+        </h1>
+      </v-col>
+      <v-col class="mt-4" lg="2">
+        <v-btn
+          elevation="5"
+          color="black"
+          class="nameBtn"
+          @click="trocaDeValores()"
+          >{{ componente }}</v-btn
+        >
+      </v-col>
+    </v-row>
+    <v-divider></v-divider>
     <component :is="componente" />
   </div>
 </template>
 
 <script>
-import cadastro from "../components/pages/produtos/cadastro.vue";
-import visualizacao from "../components/pages/produtos/visualizacao.vue";
+import Cadastrar from "../components/pages/produtos/cadastro.vue";
+import Visualizar from "../components/pages/produtos/visualizacao.vue";
 
 export default {
   components: {
-    cadastro,
-    visualizacao
-
+    Cadastrar,
+    Visualizar,
   },
 
   data: () => ({
-    componente: "cadastro",
+    componente: "Cadastrar",
   }),
+  methods: {
+    trocaDeValores() {
+      this.componente == "Cadastrar" ? this.componente = "Visualizar" : this.componente = "Cadastrar" ;
+    },
+  },
 };
 </script>
-
 
