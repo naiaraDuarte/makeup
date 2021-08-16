83f7381a38d09e89860950dafae8a5fa033defc0
naiaraDuarte <naiara.duarte123@gmail.com>
2021-08-16 07:27:20 -0300

    Gerando imagens aleatorias

--- src/components/pages/produtos/visualizacao.vue
+++ src/components/pages/produtos/visualizacao.vue
@@ -5,13 +5,13 @@
         <v-col lg="2" class="px-2" v-for="(item, i) in 10" :key="i">
           <v-card elevation="0">
             <v-img v-if="i%2 == 0"
-              src="../../../assets/images/baseRuby.jpeg"
+              src="../../../assets/images/produto1.jpeg"
               class="w-100"
               max-height="150"
               max-width="300"
             ></v-img>
             <v-img v-if="i%2 == 1"
-              src="../../../assets/images/rimel.png"
+              src="../../../assets/images/produto2.jpeg"
               class="w-100"
               max-height="150"
               max-width="300"
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,20 @@
 <template>
-  <div>
+  <div class="teste">
     <v-container fluid>
-      <v-row style="margin-top: 0px">
-        <v-col lg="2" class="px-2" v-for="(item, i) in 28" :key="i">
+      <v-row style="margin-top: 0px" v-if="itens.length > 0">
+        <v-col lg="2" class="px-2" v-for="(item, i) in itens" :key="i">
           <v-card elevation="0">
             <v-img
-              v-if="i % 2 == 0"
-              src="../assets/images/baseRuby.jpeg"
+              :src="getImgUrl(item.src)"
               class="w-100"
               max-height="150"
               max-width="300"
-            ></v-img>
-            <v-img
-              v-if="i % 2 == 1"
-              src="../assets/images/rimel.png"
-              class="w-100"
-              max-height="150"
-              max-width="300"
-            ></v-img>
+            /><v-img />
             Dados da imagem
           </v-card>
         </v-col>
       </v-row>
+      <button @click="imagens">Click aqui</button>
     </v-container>
   </div>
 </template>
@@ -29,7 +22,28 @@
 <script>
 export default {
   name: "Home",
-
+  data() {
+    return {
+      itens: [],
+    };
+  },
   components: {},
+  created() {
+    for (let index = 0; index < 15; index++) {
+      let x = Math.floor(Math.random() * 11 + 1);
+      console.log(`../assets/images/produto${x}.jpeg`);
+      this.itens.push({ src: `produto${x}.jpeg` });
+    }
+  },
+  methods: {
+    getImgUrl(pic) {
+      return require("../assets/images/" + pic);
+    },
+  },
 };
 </script>
+<style>
+.teste {
+  /* background-image: url("../assets/images/produto2.png"); */
+}
+</style>
