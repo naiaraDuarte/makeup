115c297b79fe39b8891f54281e41d0ba4f40924e
Liziane Tamm <lizianetamm@hotmail.com>
2021-09-12 20:24:42 -0300

    Cupons

--- src/components/pages/administrador/cupons.vue
+++ src/components/pages/administrador/cupons.vue
@@ -45,7 +45,41 @@
           <v-btn color="primary mr-5" @click="gerar"> Gerar Cupom </v-btn>
         </v-col>
       </v-row>
+      <h4>Cupons Cadastrados</h4>
+       <v-row class="mt-3">
+        <v-col lg="3" v-for="(item, i) in this.$store.state.cupons" :key="i">
+          <v-card elevation="0" class="card-cartao p-2">
+            
+            <v-row>
+              <v-col lg="8">
+                <v-row>
+                  <v-col lg="12" color="blue">
+                    <span>
+                      <b>{{ item.cod }}</b>
+                    </span>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col lg="12" color="red">
+                    <span
+                      ><b>{{ item.tipo }}</b></span
+                    >
+                  </v-col>
+                </v-row>
+              </v-col>
+              <v-col lg="4">
+                <span
+                  ><b>{{ item.porcen }}</b></span
+                >
+              </v-col>
+            </v-row>
+          </v-card>
+        </v-col>
+      </v-row>  
+        
+     
     </v-card>
+
     <v-snackbar v-model="snackbar" :color="snackbarColor">
       <h4 style="font-weight: 100">{{ mensagem }}</h4>
       <template v-slot:action="{ attrs }">
@@ -75,7 +109,13 @@ export default {
   methods: {
     ...mapMutations(["addCupons"]),
     gerar() {
-      if (this.cupom == "" || this.porcen == "" || this.tipoDesconto == "" || this.quant == null || this.quant == 0) {
+      if (
+        this.cupom == "" ||
+        this.porcen == "" ||
+        this.tipoDesconto == "" ||
+        this.quant == null ||
+        this.quant == 0
+      ) {
         this.exibeSnackBar("#b38b57", "Todos os campos devem ser preechidos");
         return false;
       }
@@ -83,7 +123,7 @@ export default {
         cod: this.cupom.toUpperCase(),
         porcen: parseInt(this.porcen.replace("%", "")),
         tipo: this.tipoDesconto,
-        quant: this.quant
+        quant: this.quant,
       };
       this.addCupons(frm);
       this.exibeSnackBar("green", "Cupom adicionado");
@@ -94,6 +134,10 @@ export default {
       this.mensagem = msg;
       this.snackbar = true;
     },
+    // getCupom(){
+    //   this.cupons
+
+    // },
   },
 };
 </script>
@@ -104,5 +148,14 @@ export default {
 .alinhamento {
   display: flex;
   justify-content: flex-end;
+}
+.card-cartao {
+  border: 2px solid #bbb !important;
+}
+.marcado {
+  border: 2px solid #b38b57 !important;
+}
+.desmarcado {
+  border: 2px solid #bbb !important;
 }
 </style>
